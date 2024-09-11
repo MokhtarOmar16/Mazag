c533ba3d98d1a69a17889d7eff2b59f3859143de
Mokhtar Omar <AlMokhtarOmar24@gmail.com>
2024-09-11 07:27:51 +0300

    adding follow logic to user profile

--- core/models.py
+++ core/models.py
@@ -6,16 +6,43 @@ class User(AbstractUser):
     email = models.EmailField(unique=True)
 
 
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User,on_delete = models.CASCADE, related_name='data')
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # is_private = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.user.username
+
+    def follow(self, user):
+        if self.is_following(user):
+            return 
+        return Follow.objects.create(user=user, follower_user=self)
+
+
+    def is_following(self, user):
+        return Follow.objects.filter(user=user, follower_user=self).exists()
+
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            return
+        return Follow.objects.delete(user=user, follower_user=self)
+
+
 class Follow(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
-    following_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
-    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='following')
+    follower_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='followers')
+    created_at = models.DateTimeField(auto_now_add=True)
     
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=['user_id','following_user_id'],  name="unique_followers")
         ]
-        ordering = ["-created"]
+        ordering = ["-created_at"]
         
     def __str__(self) -> str:
-        return f"{self.user} follows {self.following_user}"
+        return f"{self.follower_user} follows {self.user}"
