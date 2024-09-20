092d9b0a1124ada03c08ff7cc12fb2c54959197c
Mokhtar Omar <AlMokhtarOmar24@gmail.com>
2024-09-20 12:43:05 +0300

    database setting up in production env

--- blog/settings/prod.py
+++ blog/settings/prod.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 from .common import *
 
 
@@ -9,3 +10,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = [
     
 ]
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
