2cbe98a3fbe0f31ea4b7dd2157c0f7daf332739a
Mokhtar Omar <52607525+MokhtarOmar16@users.noreply.github.com>
2024-11-26 20:52:42 +0200

    Update common.py

--- blog/settings/common.py
+++ blog/settings/common.py
@@ -174,7 +174,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://only-freinds.vercel.app"
+    "https://only-freinds.vercel.app",
+    "https://mazag.vercel.app/"
 ]
 
 
